4271aff525447a86cd3e60d25b8478883a7ab000
kckirch <kckircher@hotmail.com>
2022-03-27 20:27:59 -0600

    Takes signup now

--- inventory_handler.php
+++ inventory_handler.php
@@ -4,10 +4,9 @@
     $dao = new Dao();
 
     #handler is causing issue where the text is not being passed into dao.php, inventory.php looks to be okay unless it is improperly taking in payload in forms,
-    $Brand = $_POST['Brand'];
-    $Model = $_POST[`Model`];
 
-    $dao->insertInventory($Brand, $Model, $_POST[`Colorway`], $_POST[`Size`], $_POST[`RetailPrice`], $_POST[`SalePrice`], $_POST[`StyleCode`], $_POST[`ItemCondition`], $_POST[`Notes`]);
+
+    $dao->insertInventory($_POST[`Brand`], $_POST[`Model`], $_POST[`Colorway`], $_POST[`Size`], $_POST[`RetailPrice`], $_POST[`SalePrice`], $_POST[`StyleCode`], $_POST[`ItemCondition`], $_POST[`Notes`]);
 
     
     header('Location: inventory.php');
